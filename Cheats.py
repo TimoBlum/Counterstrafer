64de7e3b0e7f768f726693b9e87569950d4d45c5
Timothee Blummer <59644997+TimoBlum@users.noreply.github.com>
2021-09-13 15:25:16 +0200

    keyboard layouts
    The cheat now works with AZERTY and QWERTY keyboard layouts

--- Cheats.py
+++ Cheats.py
@@ -4,11 +4,17 @@ pygame.init()
 
 run = True
 pause = False
-KOpposites = {"a": "d", "d": "a"}
 clock = pygame.time.Clock()
 atimer = 0
 dtimer = 0
 pausetimer = 0
+layout = input("Is your keyboard layout QWERTY (0) or AZERTY (1)? ")
+if int(layout) == 0:
+    left = "a"
+    right = "d"
+elif int(layout) == 1:
+    left = "q"
+    right = "d"
 
 while run:
     clock.tick(550)
@@ -18,28 +24,28 @@ while run:
             pausetimer = 200
             print("PAUSED CHEATS")
 
-        if keyboard.is_pressed("a"):
+        if keyboard.is_pressed(left):
             if atimer < 130:
                 atimer += 2
 
-        if keyboard.is_pressed("d"):
+        if keyboard.is_pressed(right):
             if dtimer < 130:
                 dtimer += 2
 
-        if keyboard.is_pressed("a") or keyboard.is_pressed("d"):
+        if keyboard.is_pressed(left) or keyboard.is_pressed(right):
             pass
 
         else:
             if atimer > 0:
-                keyboard.press("d")
+                keyboard.press(right)
                 pygame.time.wait(atimer)
-                keyboard.release("d")
+                keyboard.release(right)
                 atimer = 0
 
             if dtimer > 0:
-                keyboard.press("a")
+                keyboard.press(left)
                 pygame.time.wait(dtimer)
-                keyboard.release("a")
+                keyboard.release(left)
                 dtimer = 0
 
     else:
